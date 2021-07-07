8c6b6e8274af5eed89481603a2e86625ad6dc4f6
Hasan <hasan_c17@hotmail.com>
2021-07-07 19:53:22 -0400

    initial commit

--- src/test/java/b22_user/HasanWorking.java
+++ src/test/java/b22_user/HasanWorking.java
@@ -2,7 +2,7 @@ package b22_user;
 
 public class HasanWorking {
     public static void main(String[] args) {
-        System.out.println("hello");
+        System.out.println("hi");
         System.out.println("how r u today");
     }
 }
