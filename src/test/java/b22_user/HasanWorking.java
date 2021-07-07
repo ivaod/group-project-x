7e52705152f82d09ffd622f6bc088c12c1142fae
Hasan <hasan_c17@hotmail.com>
2021-07-07 19:51:50 -0400

    new class added

---
+++ src/test/java/b22_user/HasanWorking.java
@@ -0,0 +1,8 @@
+package b22_user;
+
+public class HasanWorking {
+    public static void main(String[] args) {
+        System.out.println("hello");
+        System.out.println("how r u today");
+    }
+}
